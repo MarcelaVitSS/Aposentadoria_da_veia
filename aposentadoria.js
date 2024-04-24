f87c13babd2b9f55a4a5ae47ef8edb19e5e87c6c
MarcelaVitSS <159553463+MarcelaVitSS@users.noreply.github.com>
2024-04-24 17:16:21 -0300

    Update aposentadoria.js

---
+++ aposentadoria.js
@@ -0,0 +1,23 @@
+const nome = "Silvana";
+const sexo = "F";
+const idade = 48;
+const contribuicao = 23;
+
+if (sexo == F) and (contribuicao>=30);
+    if (idade+contribuicao>=85){
+        console.log('Silvana, você pode se aposentar!')
+}
+    else{
+        console.log('Silvana, você ainda não pode se aposentar!')
+}
+if (sexo == M) and (contribuicao>=35);
+    if (idade+contribuicao>=95){
+        console.log('Silvana, você pode se aposentar!')
+}
+    else{
+        console.log('Silvana, você ainda não pode se aposentar!')
+}
+if (sexo == F) and (contribuicao<30);{
+    console.log('Você não é qualificado para as condiçoes')}
+if (sexo == M) and (contribuicao<35);
+    console.log('Você não é qualificado para as condiçoes')
